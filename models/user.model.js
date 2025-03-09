aa2a079b9828417a6c3819f12ba0c757c15c124a
Benfranklin m s <bfranklinms@gmail.com>
2025-03-09 09:05:04 +0530

    schema created

---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        minlength: [3, "Username must be at least 3 character long"]
+    },
+
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        minlength: [13, "Email must be at least 13 character long"],
+        },
+    password:{
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [5, "Password must be at least 5 character long"]
+    }
+
+});
