c5a8e2e8a3bf5ee9e1d849556fe18e5374d5dfd4
Benfranklin m s <bfranklinms@gmail.com>
2025-03-09 15:15:37 +0530

    add file upload functionality and update form action

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,18 @@
 import express from "express";
 const router = express.Router();
+import multer from "multer";
 
+
+const upload = multer({storage: multer.memoryStorage()});
 
 router.get("/home", (req, res) => {
     res.render("home");
+});
+
+router.post("/upload", upload.single("file"), async (req, res) => {
+    res.send(req.file);
 })
+
 
 
 
